da8ef69ceb4df8aa29877edfb2ec48a03ae07ec7
ManuelBarreiro03 <manuelbarreiro2003@gmail.com>
2023-04-21 14:24:12 -0300

    21/04/2023 14:24

---
+++ pryMiPrimerProyecto/frmPrincipal.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace pryMiPrimerProyecto
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAlimentos = new System.Windows.Forms.Button();
+            this.cmdFinancias = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdAlimentos
+            // 
+            this.cmdAlimentos.Location = new System.Drawing.Point(12, 12);
+            this.cmdAlimentos.Name = "cmdAlimentos";
+            this.cmdAlimentos.Size = new System.Drawing.Size(442, 154);
+            this.cmdAlimentos.TabIndex = 0;
+            this.cmdAlimentos.Text = "Alimentos";
+            this.cmdAlimentos.UseVisualStyleBackColor = true;
+            this.cmdAlimentos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmdFinancias
+            // 
+            this.cmdFinancias.Location = new System.Drawing.Point(12, 172);
+            this.cmdFinancias.Name = "cmdFinancias";
+            this.cmdFinancias.Size = new System.Drawing.Size(442, 165);
+            this.cmdFinancias.TabIndex = 1;
+            this.cmdFinancias.Text = "Financias";
+            this.cmdFinancias.UseVisualStyleBackColor = true;
+            this.cmdFinancias.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 349);
+            this.Controls.Add(this.cmdFinancias);
+            this.Controls.Add(this.cmdAlimentos);
+            this.Name = "frmPrincipal";
+            this.Text = "frmAlimentos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdAlimentos;
+        private System.Windows.Forms.Button cmdFinancias;
+    }
+}
---
+++ pryMiPrimerProyecto/frmPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMiPrimerProyecto
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmAlacena ventanaAlacena = new frmAlacena();
+
+            ventanaAlacena.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmFinancias ventanaFinancias = new frmFinancias();
+
+            ventanaFinancias.ShowDialog();
+        }
+    }
+}
