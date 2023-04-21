a784e64fe94c7f06cbe776939ef9d9582583c4ed
ManuelBarreiro03 <manuelbarreiro2003@gmail.com>
2023-04-21 12:55:03 -0300

    210423 1254

--- pryMiPrimerProyecto/frmAlacena.Designer.cs
+++ pryMiPrimerProyecto/frmAlacena.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmAlacena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(365, 450);
+            this.Name = "frmAlacena";
             this.Text = "frmAlacena";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
